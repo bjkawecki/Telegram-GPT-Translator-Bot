32be10640c77e94fa17b8401422184db14d01c1c
bkawecki <121129764+bjkawecki@users.noreply.github.com>
2025-05-17 01:51:09 +0200

    initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "code_bucket_name" {
+  type      = string
+  sensitive = true
+}
+
+variable "telegram_bot_token" {
+  description = "Geheimer Token für Telegram Webhook URL"
+  type        = string
+  sensitive   = true
+}
+
+variable "openai_api_key" {
+  type      = string
+  sensitive = true
+}
+
+
+variable "bot_name" {
+  type    = string
+  default = "mirrowchan_bot"
+}
