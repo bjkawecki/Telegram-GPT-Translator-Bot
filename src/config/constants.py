43d9a0efb35cc5bd5dd33604d7c78f198269076a
bkawecki <121129764+bjkawecki@users.noreply.github.com>
2025-05-19 22:52:00 +0200

    add ALLOWED_CHANNEL_ID

--- src/config/constants.py
+++ src/config/constants.py
@@ -3,8 +3,12 @@ import os
 SSM_PARAM_BOT_TOKEN = "/mirrowchan_bot/bot_token"
 SSM_PARAM_WEBHOOK_URL = "/mirrowchan_bot/api_gateway_url"
 TARGET_CHAT_ID = "@test_kanal_output"
+SOURCE_CHAT_ID = "@test_kanal_input"
 
 ENV = os.environ.get("APP_ENV", "dev")  # Fallback zu 'dev', wenn nicht gesetzt
 
 IS_PROD = ENV == "prod"
 IS_DEV = ENV == "dev"
+
+
+ALLOWED_CHANNEL_ID = SOURCE_CHAT_ID
