fdfc18390e2ca5c9f1da0e7cd3dea39b82c2e061
bkawecki <121129764+bjkawecki@users.noreply.github.com>
2025-05-19 23:06:56 +0200

    add app subfolder

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import time
 import argparse
 import json
-from src.handler import handler
-from src.telegram.test_data_loader import load_test_body
+from src.app.handler import handler
+from src.app.telegram.test_data_loader import load_test_body
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Telegram Payload Tester")
